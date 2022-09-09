217257d2f40aa3b0785a61a8fcb0e8d985af8767
Andrey Tkachenko <falko.lab@gmail.com>
2022-09-10 00:02:22 +0300

    Automatic package discovery

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='backtrader_transaq',
-    packages=find_packages(include=['backtrader_transaq',]),
+    packages=find_packages(),
     version='0.1.0',
     description='Transaq connector integration for backtrader',
     author='Andrey Tkachenko',
